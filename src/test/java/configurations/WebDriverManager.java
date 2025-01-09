96417af1c50c21a2f9017410f28ca036c964b7c5
Erick Araujo <batistasd678@gmail.com>
2025-01-09 10:34:44 -0300

    Updated webdriver to access SeleniumGrid

--- src/test/java/configurations/WebDriverManager.java
+++ src/test/java/configurations/WebDriverManager.java
@@ -3,15 +3,23 @@ package configurations;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class WebDriverManager {
     private static WebDriver driver;
     public static WebDriver setupDriver() {
         if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--start-maximized");
-            driver = new ChromeDriver(options);
+            //System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+            try{
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--start-maximized");
+                URL gridUrl = new URL("http://localhost:4444/wd/hub");
+                driver = new RemoteWebDriver(gridUrl, options);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return driver;
     }
